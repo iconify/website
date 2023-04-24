16f4617f43d29d28c1bdcce0e9f9bfb9821e926f
userquin <userquin@gmail.com>
2023-04-24 03:10:51 +0200

    chore: change `ogUrl`

--- .vitepress/constants.ts
+++ .vitepress/constants.ts
@@ -1,4 +1,4 @@
-export const ogUrl = 'https://unocss.dev/'
+export const ogUrl = 'https://iconify.design/'
 export const ogImage = `${ogUrl}og.png#1`
 export const title = 'Iconify Design'
 export const description = 'All popular icon sets, one framework'
