750be35c2cc5b93b8952080a281b861d96f2aaff
userquin <userquin@gmail.com>
2023-05-04 15:33:20 +0200

    chore: use starts with instead includes

--- .vitepress/md-env.ts
+++ .vitepress/md-env.ts
@@ -2,5 +2,5 @@ import type { MDEnv } from './markdown/metadata/types'
 
 export function excludeTransform(env: MDEnv | undefined) {
   const path = env?.relativePath
-  return path && (path === 'getting-started/index.md' || path.includes('.vitepress/theme/components/'))
+  return path && (path === 'getting-started/index.md' || path.startsWith('.vitepress/theme/components/'))
 }
