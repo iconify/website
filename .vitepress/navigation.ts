b9693209bce4dd39a52a51de815af072d51a8036
userquin <userquin@gmail.com>
2023-05-06 16:31:40 +0200

    chore: finish navbar and sidebars

--- .vitepress/navigation.ts
+++ .vitepress/navigation.ts
@@ -8,9 +8,8 @@ const DocumentationNav: DefaultTheme.NavItemWithLink[] = [
   { text: 'Iconify Types', link: '/docs/types/', activeMatch: '^/docs/types/' },
   { text: 'Tools for Developers', link: '/docs/tools/', activeMatch: '^/docs/tools/' },
   { text: 'Iconify API', link: '/docs/api/', activeMatch: '^/docs/api/' },
+  { text: 'Icons on demand', link: '/docs/icon-components/', activeMatch: '^/docs/(icon-components|iconify-icon)/' },
   { text: 'Articles', link: '/docs/articles/', activeMatch: '^/docs/articles/' },
-  { text: 'Icons on demand', link: '/docs/icon-components/', activeMatch: '^/docs/icon-components/' },
-  { text: 'Iconify Icon Web Component', link: '/docs/iconify-icon/' },
 ]
 
 const DocumentationSidebar: DefaultTheme.SidebarItem[] = [
@@ -21,9 +20,8 @@ const DocumentationSidebar: DefaultTheme.SidebarItem[] = [
   { text: 'Iconify Types', link: '/docs/types/' },
   { text: 'Tools for Developers', link: '/docs/tools/' },
   { text: 'Iconify API', link: '/docs/api/' },
-  { text: 'Articles', link: '/docs/articles/' },
   { text: 'Icons on demand', link: '/docs/icon-components/' },
-  { text: 'Iconify Icon Web Component', link: '/docs/iconify-icon/' },
+  { text: 'Articles', link: '/docs/articles/' },
 ]
 
 const SVGInCssSidebar: DefaultTheme.SidebarItem[] = [
@@ -49,6 +47,7 @@ const SVGInCssSidebar: DefaultTheme.SidebarItem[] = [
     ],
   },
 ]
+
 const SVGInHtmlSidebar: DefaultTheme.SidebarItem[] = [
   { text: 'Getting Started', link: '/getting-started/' },
   { text: 'Iconify documentation', link: '/docs/' },
@@ -65,6 +64,82 @@ const SVGInHtmlSidebar: DefaultTheme.SidebarItem[] = [
       }, {
         text: 'Generate SVG with Iconify Utils',
         link: '/docs/usage/svg/utils/',
+      },
+    ],
+  },
+]
+
+const WebComponentSidebar: DefaultTheme.SidebarItem[] = [
+  { text: 'Getting Started', link: '/getting-started/' },
+  { text: 'Iconify documentation', link: '/docs/' },
+  { text: 'Icons on demand', link: '/docs/icon-components/' },
+  {
+    text: 'Web Component',
+    items: [
+      {
+        text: 'Wrappers for:',
+        items: [{
+          text: 'React',
+          link: '/docs/iconify-icon/react',
+        }, {
+          text: 'Solid',
+          link: '/docs/iconify-icon/solid',
+        }],
+      }, {
+        text: 'Attributes:',
+        items: [{
+          text: 'Icon Color',
+          link: '/docs/iconify-icon/color',
+        }, {
+          text: 'Icon Dimensions',
+          link: '/docs/iconify-icon/dimensions',
+        }, {
+          text: 'Transformations',
+          link: '/docs/iconify-icon/transform',
+        }, {
+          text: 'Vertical Alignment',
+          link: '/docs/iconify-icon/inline',
+        }, {
+          text: 'Rendering Modes',
+          link: '/docs/iconify-icon/modes',
+        }],
+      }, {
+        text: 'Use Without API',
+        link: '/docs/iconify-icon/without-api',
+      }, {
+        text: 'Functions:',
+        collapsed: true,
+        items: [{
+          text: 'iconExists()',
+          link: '/docs/iconify-icon/icon-exists',
+        }, {
+          text: 'listIcons()',
+          link: '/docs/iconify-icon/list-icons',
+        }, {
+          text: 'getIcon()',
+          link: '/docs/iconify-icon/get-icon',
+        }, {
+          text: 'addCollection()',
+          link: '/docs/iconify-icon/add-collection',
+        }, {
+          text: 'loadIcons()',
+          link: '/docs/iconify-icon/load-icons',
+        }, {
+          text: 'loadIcon()',
+          link: '/docs/iconify-icon/load-icon',
+        }, {
+          text: 'enableCache()',
+          link: '/docs/iconify-icon/enabled-cache',
+        }, {
+          text: 'disableCache()',
+          link: '/docs/iconify-icon/disabled-cache',
+        }, {
+          text: 'buildIcon()',
+          link: '/docs/iconify-icon/build-icon',
+        }, {
+          text: 'setFetch()',
+          link: '/docs/iconify-icon/set-fetch',
+        }],
       },
     ],
   },
@@ -91,31 +166,33 @@ const DocumentationSidebars: Record<string, DefaultTheme.SidebarItem[]> = {
     link: '/docs/articles/how-iconify-works',
   }],
   '/docs/icon-components/': [{
-    text: 'Vue 2',
-    link: '/docs/icon-components/vue2/',
+    text: 'Web Component',
+    link: '/docs/iconify-icon/',
+  }, {
+    text: 'SVG Framework',
+    link: '/docs/icon-components/svg-framework/',
+  }, {
+    text: 'React',
+    link: '/docs/icon-components/react/',
   }, {
     text: 'Vue 3',
     link: '/docs/icon-components/vue/',
+  }, {
+    text: 'Vue 2',
+    link: '/docs/icon-components/vue2/',
   }, {
     text: 'Svelte',
     link: '/docs/icon-components/svelte/',
   }, {
     text: 'Ember',
     link: '/docs/icon-components/ember/',
-  }, {
-    text: 'React',
-    link: '/docs/icon-components/react/',
-  }, {
+  }, /* , {
     text: 'React with API',
     link: '/docs/icon-components/react-with-api/',
-  }, {
-    text: 'Iconify SVG Framework',
-    link: '/docs/icon-components/svg-framework/',
-  }, {
-    text: 'Iconify Icon Bundles',
+  } */ {
+    text: 'Icon Bundles',
     link: '/docs/icon-components/bundles/',
   }],
-  '/docs/iconify-icon/': [],
   '/docs/icons/': [{
     text: 'Icon Basics',
     link: '/docs/icons/icon-basics',
@@ -248,7 +325,7 @@ export const GlobalSidebar: DefaultTheme.Sidebar = {
   '/docs/api/': sideBarForDocumentation('/docs/api/'),
   '/docs/articles/': sideBarForDocumentation('/docs/articles/'),
   '/docs/icon-components/': sideBarForDocumentation('/docs/icon-components/'),
-  '/docs/iconify-icon/': sideBarForDocumentation('/docs/iconify-icon/'),
+  '/docs/iconify-icon/': WebComponentSidebar,
   '/docs/icons/': sideBarForDocumentation('/docs/icons/'),
   '/docs/tools/': sideBarForDocumentation('/docs/tools/'),
   '/docs/types/': sideBarForDocumentation('/docs/types/'),
