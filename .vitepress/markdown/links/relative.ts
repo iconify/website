5cd1cdd0cc7caadd7b8b346c2c0e5b24c9c57016
Vjacheslav Trushkin <cyberalien@gmail.com>
2023-06-12 15:19:52 +0300

    chore: normalise relative links on Windows

--- .vitepress/markdown/links/relative.ts
+++ .vitepress/markdown/links/relative.ts
@@ -1,6 +1,6 @@
 import { dirname, join } from 'node:path'
 
 export function getAbsoluteLink(href: string, file: string): string {
-  const target = join(dirname(file), href)
+  const target = join(dirname(file), href).replace(/\\/g, '/')
   return `/${target}`
 }
