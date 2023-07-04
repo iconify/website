471bf01aeeb010fe804e2fd39f682c61b785dfdd
Vjacheslav Trushkin <cyberalien@gmail.com>
2023-07-04 09:11:14 +0300

    chore: fix typo in tools code sample

--- code-samples/libraries/tools/import/json-min.ts
+++ code-samples/libraries/tools/import/json-min.ts
@@ -1,20 +1,20 @@
 import { promises as fs } from 'fs';
-import { IconSet, cleanupSVG } from '@iconify/tools';
+import { IconSet, cleanupSVG, exportToDirectory } from '@iconify/tools';
 import { validateIconSet } from '@iconify/utils';
 
 (async () => {
 	// Read data, parse JSON
 	const rawData = JSON.parse(
-		await fs.readFile('files/arty-animated.svg', 'utf8')
+		await fs.readFile('files/arty-animated.json', 'utf8')
 	);
 
-	// Validate icon set
+	// Validate icon set (optional step)
 	const validatedData = validateIconSet(rawData);
 
 	// Create new IconSet instance
 	const iconSet = new IconSet(validatedData);
 
-	// Clean up icons
+	// Clean up icons (optional step)
 	iconSet.forEachSync(
 		(name) => {
 			const svg = iconSet.toSVG(name);
@@ -40,4 +40,8 @@ import { validateIconSet } from '@iconify/utils';
 	);
 
 	// Done. Do other stuff...
+	// For example, export as SVG files
+	await exportToDirectory(iconSet, {
+		target: 'output'
+	})
 })();
